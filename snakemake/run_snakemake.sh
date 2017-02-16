f90bf8825af2dcaf4bae0eacd77d68529876b93b
Maurits Evers <maurits.evers@anu.edu.au>
2017-02-16 16:02:49 +1100

    Added simulate_ChIP_reads rule and extended Snakemake workflow

--- snakemake/run_snakemake.sh
+++ snakemake/run_snakemake.sh
@@ -3,18 +3,5 @@
 snakemake --dag | dot -Tpdf > dag.pdf
 snakemake --rulegraph | dot -Tpdf > rulegraph.pdf
 
-snakemake --configfile config.yaml \
-	  --snakefile Snakefile \
-	  --jobs 10 \
-	  --cluster "qsub -pe threads {cluster.threads} \
-                          -q {cluster.queue} \
-                          -l virtual_free={cluster.virtual_free} \
-                          -l h_vmem={cluster.h_vmem} \
-                          -o {cluster.outstream} \
-                          -e {cluster.errorstream}" \
-          --cluster-config cluster.yaml
-
-
-#                          -e {cluster.error_out_dir} \
-#                          -o {cluster.std1_out_dir} \
-
+snakemake -p --configfile config.yaml \
+	  --snakefile Snakefile
