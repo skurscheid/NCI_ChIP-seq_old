e1be5f873fac1705751079867c03c21aeb01eb37
Maurits Evers <maurits.evers@anu.edu.au>
2017-03-01 02:51:25 +1100

    Fixed error in ref seq links; added cluster config

--- snakemake/run_snakemake.sh
+++ snakemake/run_snakemake.sh
@@ -4,4 +4,14 @@ snakemake --dag | dot -Tpdf > dag.pdf
 snakemake --rulegraph | dot -Tpdf > rulegraph.pdf
 
 snakemake -p --configfile config.yaml \
-	  --snakefile Snakefile
+	  --snakefile Snakefile \
+	  --jobs 10 \
+	  --printshellcmds \
+	  --cluster "qsub -pe threads {cluster.threads} \
+                          -q {cluster.queue} \
+                          -l virtual_free={cluster.virtual_free} \
+                          -l h_vmem={cluster.h_vmem} \
+                          -o {cluster.outstream} \
+                          -e {cluster.errorstream}" \
+          --cluster-config cluster.yaml
+	  
